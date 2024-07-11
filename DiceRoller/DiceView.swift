85e3b3dbcbc2aa5554eb426de2db92bbbf891d92
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2024-07-11 13:39:09 -0400

    Finished the app

---
+++ DiceRoller/DiceView.swift
@@ -0,0 +1,18 @@
+//
+//  DiceView.swift
+//  DiceRoller
+//
+//  Created by Jordan Haynes on 7/11/24.
+//
+
+import SwiftUI
+
+struct DiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiceView()
+}
