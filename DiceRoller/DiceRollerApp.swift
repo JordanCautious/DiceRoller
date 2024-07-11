5d4e25dfd3c0004fb9b72732823872758676c042
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2024-07-11 13:15:02 -0400

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Jordan Haynes on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
